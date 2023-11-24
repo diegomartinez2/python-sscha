f29ea8c766f5f7c73e931bf335d49b446258ab49
diegomartinez2 <diegomartinez2@yahoo.es>
2023-11-24 08:28:09 +0100

    OPM pararelization of "Rotate displacements"

--- SCHAModules/get_v4.f90
+++ SCHAModules/get_v4.f90
@@ -11,7 +11,7 @@ subroutine get_v4 ( a, er, transmode, amass, ityp_sc, f, u, rho, log_err, v4, &
     use stochastic
 
     implicit none
-  
+
     double precision, dimension(n_mode), intent(in) :: a
     double precision, dimension(nat_sc,n_mode,3), intent(in) :: er
     logical, dimension(n_mode), intent(in) :: transmode
@@ -21,26 +21,26 @@ subroutine get_v4 ( a, er, transmode, amass, ityp_sc, f, u, rho, log_err, v4, &
     double precision, dimension(n_random), intent(in) :: rho
     character (len=10), intent(in) :: log_err
     double precision, dimension(n_mode,n_mode,n_mode, n_mode), intent(out) :: v4
-  
+
     integer :: nat_sc, n_mode, n_mode2, n_random, ntyp
 
     ! Here the variables for this method
-  
+
     double precision, dimension(:,:), allocatable :: e
     double precision, dimension(:), allocatable :: fun
-    double precision, dimension(:,:), allocatable :: ur, u2, f2 
-    double precision :: av, av_err 
-    
+    double precision, dimension(:,:), allocatable :: ur, u2, f2
+    double precision :: av, av_err
+
     integer :: i, j, mu, nu, alpha, beta
     integer :: ka, ja
     integer :: x, y, z, w, thread_num
 
     logical, parameter :: debug = .true.
-  
+
     real :: t1, t2
-  
+
     ! Get integers
-  
+
     !nat_sc = size(er(:,1,1))
     !n_mode = 3*nat_sc
     n_mode2 = n_mode*n_mode
@@ -51,86 +51,91 @@ subroutine get_v4 ( a, er, transmode, amass, ityp_sc, f, u, rho, log_err, v4, &
         print *, "=== V4 DEBUG ==="
         print *, "N_MODE:", n_mode
         print *, "N_RANDOM:", n_random
-        print *, "NAT_SC:", nat_sc 
+        print *, "NAT_SC:", nat_sc
         call flush()
     end if
-    
-  
+
+
     ! Allocate stuff
-  
+
     allocate(e(n_mode,n_mode))
     allocate(ur(n_random,n_mode))
     allocate(u2(n_random,n_mode))
     allocate(f2(n_random,n_mode))
     allocate(fun(n_random))
-  
+
     ! Calculate e polarization vectors with lengths and masses
 
     if (debug) then
         print *, "BEFORE EMAT"
         call flush()
-    endif  
-  
-    call get_emat (er, a, amass, ityp_sc, .false., transmode, e, n_mode, nat_sc, ntyp) 
-  
+    endif
+
+    call get_emat (er, a, amass, ityp_sc, .false., transmode, e, n_mode, nat_sc, ntyp)
+
     ! Get displacements in a rank two tensor
 
     if (debug) then
         print *, "AFTER EMAT"
         call flush()
-    endif  
-  
+    endif
+
     ka = 0
-  
+
     do i = 1, nat_sc
       do alpha = 1, 3
         ka = ka + 1
         u2(:,ka) = u(:,i,alpha)
         f2(:,ka) = f(:,i,alpha)
       end do
-    end do  
-  
+    end do
+
     ! Rotate displacementes
-  
+  !$OMP PARALLEL DO PRIVATE(mu, y, temp)
     do x = 1, n_mode
       ur(:,x) = 0.0d0
       do mu = 1, n_mode
+        temp = 0.0d0
+        !$OMP DO REDUCTION(+:temp)
         do y = 1, n_mode
-          ur(:,x) = ur(:,x) + u2(:,y)*e(mu,x)*e(mu,y)
+          temp = temp + u2(:,y)*e(mu,x)*e(mu,y)
+!!!          ur(:,x) = ur(:,x) + u2(:,y)*e(mu,x)*e(mu,y)
         end do
+        !$OMP END DO
+        ur(:,x) = ur(:,x) + temp
       end do
     end do
-  
+  !$OMP END PARALLEL DO
+
     ! Calculate the third order coefficients
-  
+
     !thread_num = omp_get_max_threads ( )
 
     if (debug) then
         print *, "BEFORE SUPERLOOP"
         call flush()
-    endif  
-  
+    endif
+
     !$omp parallel SHARED ( v4, ur, f2, rho, n_mode, log_err ) PRIVATE ( x, y, z, w, fun, av, av_err )
     !$omp do COLLAPSE(4)
     do x = 1, n_mode
       do y = 1, n_mode
-        do z = 1, n_mode 
-          do w = 1, n_mode 
+        do z = 1, n_mode
+          do w = 1, n_mode
             fun(:) = - ur(:,x) * ur(:,y) * ur(:,z) * f2(:,w)
             call average_error_weight(fun,rho,log_err,av,av_err)
-            v4(x,y,z,w) = av 
+            v4(x,y,z,w) = av
           end do
         end do
       end do
     end do
     !$omp end do
     !$omp end parallel
-  
+
     if (debug) then
         print *, "AFTER SUPERLOOP"
         call flush()
-    endif  
+    endif
     deallocate(e,ur,u2,f2,fun)
-  
+
   end subroutine get_v4
-  
